11817ed3b7ad9e69ddd80a6beff547db0704a59d
victorioliay <victorioliay@gmail.com>
2023-06-20 12:10:48 -0300

    Update R&M express

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 const PORT = 3001;
-const router = require("./routes/index");
+const { router } = require("./routes/index");
 
 server.use(express.json());
 
