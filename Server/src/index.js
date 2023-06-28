218edf4feb9fda1c85cf116417f336495a6f74fa
victorioliay <victorioliay@gmail.com>
2023-06-28 19:26:58 -0300

    uptdate R&M

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,8 +1,8 @@
+const { conn } = require("./DB_connection");
 const server = require("./app");
 const PORT = 3001;
-const { conn } = require("./DB_connection");
 
-server.listen(PORT, async () => {
-  await conn.sync({ force: true });
+server.listen(PORT, () => {
+  conn.sync({ force: true });
   console.log(`server listening on port: ${PORT}`);
 });
