dbff0cecdb390665cb69d4e086518d191b5058b3
victorioliay <victorioliay@gmail.com>
2023-06-28 17:45:20 -0300

    sequelize R&M 1° update

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,8 @@
 const server = require("./app");
 const PORT = 3001;
+const { conn } = require("./DB_connection");
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await conn.sync({ force: true });
   console.log(`server listening on port: ${PORT}`);
 });
