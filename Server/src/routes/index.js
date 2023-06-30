245ddb2815faf068f88d1911c05ab2d3ccfa133c
victorioliay <victorioliay@gmail.com>
2023-06-30 17:21:30 -0300

    R&M hw 02 sequelize done

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,22 +1,26 @@
-const { login } = require("../controllers/login");
 const { getCharById } = require("../controllers/getCharById");
-const { postFav, deleteFav } = require("../controllers/handleFavorites");
+const { login } = require("../controllers/login");
+const { postUser } = require("../controllers/postUser");
+const { postFav } = require("../controllers/postFav");
+const { deleteFav } = require("../controllers/deleteFav");
 const { Router } = require("express");
 
 const router = Router();
 
+router.get("/login", login);
+
+router.post("/login", postUser);
+
+router.post("/fav", postFav);
+
 router.get("/character/:id", getCharById);
+
+router.delete("/fav/:id", deleteFav);
 
 // esta seria la forma base o simple de hacer las rutas.
 // router.get("/character/:id", (req, res) => {
 //   getCharById(req, res);
 // });
-
-router.get("/login", login);
-
-router.post("/fav", postFav);
-
-router.delete("/fav/:id", deleteFav);
 
 module.exports = {
   router,
